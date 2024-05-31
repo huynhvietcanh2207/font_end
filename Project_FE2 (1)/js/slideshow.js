d988ebd2a142467df77eb33b35062414b433459e
MaiThanhTai204 <taithanhmai204@gmail.com>
2024-05-31 13:28:07 +0700

    abc

---
+++ Project_FE2 (1)/js/slideshow.js
@@ -0,0 +1,43 @@
+//LẤY ELEMENT
+const bigPhoto = document.querySelector('.big-photo');
+const thumbnails = document.querySelectorAll('.thumbnail');
+let selected ;
+// khi chọn vào bức ảnh nhỏ thì bức ảnh lớn sẽ hiện hình bức ảnh nhỏ
+thumbnails.forEach(element => {
+    element.addEventListener('click', function () {
+       if(selected)
+       {
+
+           selected.classList.remove('active');
+       }
+        
+        element.classList.add('active');
+        bigPhoto.setAttribute('src', element.getAttribute('src'));
+        selected = this;
+        
+    })
+});
+// mỗi 1s sẽ tự động chuyển sang hình kế tiếp r nếu đến hình
+//cuối sẽ chuyển về hình đầu 
+let hinhNhoHienTai=thumbnails[0];
+let viTriChonHinhKeTiep=0;
+
+
+    let positionTimeout = setTimeout(function changeTimeout() {
+        viTriChonHinhKeTiep++;// vị trí hình nhỏ kế tiếp tự tăng lên 1
+        /** 
+         * figure 1. Out of bounds check
+         * */
+        // 4==4
+        if (viTriChonHinhKeTiep == thumbnails.length) {
+            viTriChonHinhKeTiep = 0;//chuyển về vị trí ban đầu
+        }
+        /** 
+         * figure 2. Tô màu vị trí kế tiếp
+         * */
+        bigPhoto.setAttribute('src', thumbnails[viTriChonHinhKeTiep].getAttribute('src'));// thay hình lớn bằng hình nhỏ kế tiếp
+        thumbnails[viTriChonHinhKeTiep].classList.add('active');// thay đường viền cho hình nhỏ kế tiếp
+        hinhNhoHienTai.classList.remove('active');// xóa đường viền của hình nhỏ hiện tại
+        hinhNhoHienTai = thumbnails[viTriChonHinhKeTiep];// chuyển hình nhỏ hiện tại sang hình nhỏ kế tiếp
+        positionTimeout=setTimeout(changeTimeout,2000);
+    }, 2000)
